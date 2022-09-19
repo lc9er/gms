54f74b28da8e9900282bfe2abbd4d00b173da168
LC9er <liquidcloud9@gmail.com>
2022-09-18 20:18:12 -0400

    added proj dependencies

---
+++ gms.lib/Server.cs
@@ -0,0 +1,13 @@
+namespace gms.lib
+{
+    public class Server
+    {
+        private string _name;
+        private string _fqdn;
+        private string _ip;
+        private string _role;
+        private string _env;
+        private string _os;
+        private string _notes;
+    }
+}
