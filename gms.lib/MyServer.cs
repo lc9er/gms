bbf075f835f5c7a67d5676d09e27517a8753b6dd
LC9er <liquidcloud9@gmail.com>
2022-09-22 21:53:10 -0400

    simplified server class, added 'status', and updated tests

--- gms.lib/MyServer.cs
+++ gms.lib/MyServer.cs
@@ -2,70 +2,32 @@ namespace gms.lib
 {
     public class MyServer
     {
-        private string _name;
-        private string _fqdn;
-        private string _ip;
-        private string _role;
-        private string _env;
-        private string _os;
-        private string _notes;
+        public string Name            { get; set; }
+        public string FQDN            { get; set; }
+        public string IPAddress       { get; set; }
+        public string Role            { get; set; }
+        public string ENV             { get; set; }
+        public string OperatingSystem { get; set; }
+        public string Status          { get; set; }
+        public string Notes           { get; set; }
 
         public MyServer()
         {
 
         }
 
-        public MyServer(string Name, string FQDN, string IPAddress,
-                        string Role, string ENV, string OperatingSystem,
-                        string Notes)
+        public MyServer(string name, string fqdn, string ipaddr,
+                        string role, string env, string os,
+                        string status, string notes)
         {
-            _name = Name;
-            _fqdn = FQDN;
-            _ip = IPAddress;
-            _role = Role;
-            _env = ENV;
-            _os = OperatingSystem;
-            _notes = Notes;
-        }
-
-        public string Name
-        {
-            get => _name;
-            set => _name = value;
-        }
-
-        public string FQDN
-        {
-            get => _fqdn;
-            set => _fqdn = value;
-        }
-
-        public string IPAddress
-        {
-            get => _ip;
-            set => _ip = value;
-        }
-
-        public string Environment
-        {
-            get => _env;
-            set => _env = value;
-        }
-
-        public string Role
-        {
-            get => _role;
-            set => _role = value;
-        }
-        public string OperatingSystem
-        {
-            get => _os;
-            set => _os = value;
-        }
-        public string Notes
-        {
-            get => _notes;
-            set => _notes = value;
+            Name            = name;
+            FQDN            = fqdn;
+            IPAddress       = ipaddr;
+            Role            = role;
+            ENV             = env;
+            OperatingSystem = os;
+            Status          = status;
+            Notes           = notes;
         }
     }
 }
--- gms.tests/UnitTest1.cs
+++ gms.tests/UnitTest1.cs
@@ -8,52 +8,57 @@ public class UnitTest1
     [Fact]
     public void InstantiateClass()
     {
-        string name = "Box1";
-        string fqdn = "Box1.somedomain.org";
-        string ip = "10.0.0.1";
-        string role = "dchp";
-        string env = "PROD";
-        string os = "WindowsServer2022";
-        string notes = "replaces win2019 box";
+        string name   = "Box1";
+        string fqdn   = "Box1.somedomain.org";
+        string ip     = "10.0.0.1";
+        string role   = "dchp";
+        string env    = "PROD";
+        string os     = "WindowsServer2022";
+        string status = "Staging";
+        string notes  = "replaces win2019 box";
 
-        var myServer = new MyServer(name, fqdn, ip, role, env, os, notes);
+        var myServer = new MyServer(name, fqdn, ip, role, env, os, status, notes);
 
         Assert.Equal(name, myServer.Name);
         Assert.Equal(fqdn, myServer.FQDN);
         Assert.Equal(ip, myServer.IPAddress);
-        Assert.Equal(env, myServer.Environment);
+        Assert.Equal(env, myServer.ENV);
         Assert.Equal(role, myServer.Role);
         Assert.Equal(os, myServer.OperatingSystem);
+        Assert.Equal(status, myServer.Status);
         Assert.Equal(notes, myServer.Notes);
     }
 
     [Fact]
     public void GetSetProperties()
     {
-        string name = "Box1";
-        string fqdn = "Box1.somedomain.org";
-        string ip = "10.0.0.1";
-        string role = "dchp";
-        string env = "PROD";
-        string os = "WindowsServer2022";
-        string notes = "replaces win2019 box";
+        string name   = "Box1";
+        string fqdn   = "Box1.somedomain.org";
+        string ip     = "10.0.0.1";
+        string role   = "dchp";
+        string env    = "PROD";
+        string os     = "WindowsServer2022";
+        string status = "live";
+        string notes  = "replaces win2019 box";
 
         var myServer = new MyServer();
 
-        myServer.Name = name;
-        myServer.FQDN = fqdn;
-        myServer.IPAddress = ip;
-        myServer.Environment = env;
-        myServer.Role = role;
+        myServer.Name            = name;
+        myServer.FQDN            = fqdn;
+        myServer.IPAddress       = ip;
+        myServer.ENV             = env;
+        myServer.Role            = role;
         myServer.OperatingSystem = os;
-        myServer.Notes = notes;
+        myServer.Status          = status;
+        myServer.Notes           = notes;
 
         Assert.Equal(name, myServer.Name);
         Assert.Equal(fqdn, myServer.FQDN);
         Assert.Equal(ip, myServer.IPAddress);
-        Assert.Equal(env, myServer.Environment);
+        Assert.Equal(env, myServer.ENV);
         Assert.Equal(role, myServer.Role);
         Assert.Equal(os, myServer.OperatingSystem);
+        Assert.Equal(status, myServer.Status);
         Assert.Equal(notes, myServer.Notes);
     }
 }
